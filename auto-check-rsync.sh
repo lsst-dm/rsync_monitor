890caff05cd5f9f8cbb78da4ad518eb120ce48a1
Eric Morganson <ericm@illinois.edu>
2019-10-11 11:19:20 -0500

    minor path updates in crontab script

--- auto-check-rsync.sh
+++ auto-check-rsync.sh
@@ -8,6 +8,6 @@ fi
 setup lsst_distrib
 today=`date +"%Y-%m-%d"`
 threedaysago=`date -d '-3day' +"%Y-%m-%d"`
+$1/monitor_rsync.py -c $1/config/auxTel_DAQ_monitor.yaml $threedaysago $today
 $1/monitor_rsync.py -c $1/config/auxTel_L1Archiver_monitor.yaml $threedaysago $today
-$1/monitor_rsync.py -c $1/config/auxTel_L1Archiver_monitor.yaml $threedaysago $today
-$1/monitor_rsync.py -c $1/config/auxTel_L1Archiver_monitor.yaml $threedaysago $today
+$1/monitor_rsync.py -c $1/config/bot_monitor.yaml $threedaysago $today
