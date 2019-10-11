9f3cecbb31b64f93e7c841978b20b927f7aa90db
Eric Morganson <ericm@illinois.edu>
2019-10-11 15:56:43 -0500

    added export commands to auto-check-rsync

--- auto-check-rsync.sh
+++ auto-check-rsync.sh
@@ -5,6 +5,9 @@ if [ "$#" -ne 1 ]; then
     echo "This script is desinged to be a one-liner that inserts into your crontab."
     exit 1
 fi 
+source /software/lsstsw/stack3/loadLSST.bash
+export RSYMON_DB_ROOT=${HOME}/public_html/rsync_monitor/db
+export RSYMON_HTML_ROOT=${HOME}/public_html/rsync_monitor
 setup lsst_distrib
 today=`date +"%Y-%m-%d"`
 threedaysago=`date -d '-3day' +"%Y-%m-%d"`
